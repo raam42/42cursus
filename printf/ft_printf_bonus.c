de37d81945174c1ada3b0d8717791e3a74e8f5ee
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-04-30 20:48:56 +0200

    Create ft_printf_bonus.c

---
+++ printf/ft_printf_bonus.c
@@ -0,0 +1,41 @@
+#include "ft_printf_bonus.h"
+
+int    ft_printf(const char *format, ...)
+{
+    va_list        args;
+    t_format        fmt;
+    int            total;
+    int            i;
+    int            j;
+
+    if (!format)
+        return (-1);
+    va_start(args, format);
+    total = 0;
+    i = 0;
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            init_format(&fmt);
+            j = parse_format(format, i + 1, &fmt);
+            if (j < 0)
+            {
+                ft_putchar('%');
+                total++;
+                i++;
+                continue ;
+            }
+            total += handle_conversion_bonus(args, &fmt);
+            i = j + 1;
+        }
+        else
+        {
+            ft_putchar(format[i]);
+            total++;
+            i++;
+        }
+    }
+    va_end(args);
+    return (total);
+}
