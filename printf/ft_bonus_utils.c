80908bb0ed1377d18f4d2ec5b6f5efb116f34a75
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-04-30 23:23:44 +0200

    Update and rename ft_handle_conversion_bonus.c to ft_bonus_utils.c

---
+++ printf/ft_bonus_utils.c
@@ -0,0 +1,50 @@
+#include "ft_printf_bonus.h"
+
+int    ft_max(int a, int b)
+{
+    if (a > b)
+        return (a);
+    return (b);
+}
+
+int    put_char_count(char c)
+{
+    ft_putchar(c);
+    return (1);
+}
+
+int    putnchar(char c, int n)
+{
+    int    count;
+
+    count = 0;
+    while (n-- > 0)
+        count += put_char_count(c);
+    return (count);
+}
+
+int    putnstr(const char *s, int n)
+{
+    int    count;
+
+    count = 0;
+    while (s && *s && n-- > 0)
+    {
+        count += put_char_count(*s);
+        s++;
+    }
+    return (count);
+}
+
+int    dec_len_unsigned(unsigned long n)
+{
+    int    len;
+
+    len = 1;
+    while (n >= 10)
+    {
+        n /= 10;
+        len++;
+    }
+    return (len);
+}
