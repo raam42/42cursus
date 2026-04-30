03c9b3e01b1b11b78eecfac49ba1a87304d41679
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-04-30 22:31:06 +0200

    Update checker.c

--- push_swap/checker.c
+++ push_swap/checker.c
@@ -1,32 +1,39 @@
 #include "push_swap.h"
 
-static int    exec_instruction(char *line, t_stack_node **a, t_stack_node **b)
+static int    exec_instruction(char *line,
+                             t_stack_node **a,
+                             t_stack_node **b)
 {
     if (!ft_strncmp(line, "sa\n", 3))
-        sa(a, 1);
-    else if (!ft_strncmp(line, "sb\n", 3))
-        sb(b, 1);
-    else if (!ft_strncmp(line, "ss\n", 3))
-        ss(a, b, 1);
-    else if (!ft_strncmp(line, "pa\n", 3))
-        pa(a, b, 1);
-    else if (!ft_strncmp(line, "pb\n", 3))
-        pb(a, b, 1);
-    else if (!ft_strncmp(line, "ra\n", 3))
-        ra(a, 1);
-    else if (!ft_strncmp(line, "rb\n", 3))
-        rb(b, 1);
-    else if (!ft_strncmp(line, "rr\n", 3))
-        rr(a, b, 1);
-    else if (!ft_strncmp(line, "rra\n", 4))
-        rra(a, 1);
-    else if (!ft_strncmp(line, "rrb\n", 4))
-        rrb(b, 1);
-    else if (!ft_strncmp(line, "rrr\n", 4))
-        rrr(a, b, 1);
-    else
-        return (0);
-    return (1);
+        return (sa(a, 1), 1);
+    if (!ft_strncmp(line, "sb\n", 3))
+        return (sb(b, 1), 1);
+    if (!ft_strncmp(line, "ss\n", 3))
+        return (ss(a, b, 1), 1);
+    if (!ft_strncmp(line, "pa\n", 3))
+        return (pa(a, b, 1), 1);
+    if (!ft_strncmp(line, "pb\n", 3))
+        return (pb(a, b, 1), 1);
+    if (!ft_strncmp(line, "ra\n", 3))
+        return (ra(a, 1), 1);
+    if (!ft_strncmp(line, "rb\n", 3))
+        return (rb(b, 1), 1);
+    if (!ft_strncmp(line, "rr\n", 3))
+        return (rr(a, b, 1), 1);
+    if (!ft_strncmp(line, "rra\n", 4))
+        return (rra(a, 1), 1);
+    if (!ft_strncmp(line, "rrb\n", 4))
+        return (rrb(b, 1), 1);
+    if (!ft_strncmp(line, "rrr\n", 4))
+        return (rrr(a, b, 1), 1);
+    return (0);
+}
+
+static void    free_and_error(t_stack_node *a, t_stack_node *b)
+{
+    free_stack(a);
+    free_stack(b);
+    write(2, "Error\n", 6);
 }
 
 int    main(int argc, char **argv)
@@ -44,16 +51,13 @@ int    main(int argc, char **argv)
     if (!a)
         return (write(2, "Error\n", 6), 1);
     b = NULL;
-    while ((line = get_next_line(0)))
+    line = get_next_line(0);
+    while (line)
     {
         if (!exec_instruction(line, &a, &b))
-        {
-            free(line);
-            free_stack(a);
-            free_stack(b);
-            return (write(2, "Error\n", 6), 1);
-        }
+            return (free(line), free_and_error(a, b), 1);
         free(line);
+        line = get_next_line(0);
     }
     if (stack_sorted(a) && !b)
         write(1, "OK\n", 3);
