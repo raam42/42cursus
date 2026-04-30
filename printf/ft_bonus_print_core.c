79e0ff3c9aa6249c8a0019635d8732dd731984e3
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-04-30 23:25:38 +0200

    Create ft_bonus_print_core.c

---
+++ printf/ft_bonus_print_core.c
@@ -0,0 +1,41 @@
+int    print_char_bonus(int c, t_format *f)
+{
+    int    pad;
+    int    count;
+
+    pad = 0;
+    if (f->width > 1)
+        pad = f->width - 1;
+    count = 0;
+    if (!f->minus)
+        count += putnchar(' ', pad);
+    count += put_char_count((char)c);
+    if (f->minus)
+        count += putnchar(' ', pad);
+    return (count);
+}
+
+int    print_str_bonus(char *s, t_format *f)
+{
+    int    len;
+    int    out_len;
+    int    pad;
+    int    count;
+
+    if (!s)
+        s = "(null)";
+    len = (int)ft_strlen(s);
+    out_len = len;
+    if (f->dot && f->precision < out_len)
+        out_len = f->precision;
+    pad = 0;
+    if (f->width > out_len)
+        pad = f->width - out_len;
+    count = 0;
+    if (!f->minus)
+        count += putnchar(' ', pad);
+    count += putnstr(s, out_len);
+    if (f->minus)
+        count += putnchar(' ', pad);
+    return (count);
+}
