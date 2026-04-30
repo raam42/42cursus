7a558001bd87d2bc2502a324e54e9aadaf239bef
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-04-30 23:36:50 +0200

    Update ft_bonus_layout2.c

--- printf/ft_bonus_layout2.c
+++ printf/ft_bonus_layout2.c
@@ -1,7 +1,35 @@
 #include "ft_printf_bonus.h"
 
+static char    get_padc(t_format *f)
+{
+    if (f->zero && !f->minus && !f->dot)
+        return ('0');
+    return (' ');
+}
+
+static int    get_zprec(t_format *f, int dlen)
+{
+    int    zprec;
+
+    zprec = 0;
+    if (f->dot)
+        zprec = ft_max(0, f->precision - dlen);
+    return (zprec);
+}
+
+static int    print_hex_body(unsigned long nb, const char *base, int dlen, int zprec)
+{
+    int    count;
+
+    count = 0;
+    count += putnchar('0', zprec);
+    if (dlen > 0)
+        count += put_unsigned_base(nb, base, 16);
+    return (count);
+}
+
 int    print_hex_layout(unsigned long nb, const char *base,
-                        const char *pre, int pre_len, t_format *f)
+        const char *pre, int pre_len, t_format *f)
 {
     int        dlen;
     int        zprec;
@@ -12,12 +40,8 @@ int    print_hex_layout(unsigned long nb, const char *base,
     dlen = ulen_base(nb, 16);
     if (f->dot && f->precision == 0 && nb == 0)
         dlen = 0;
-    zprec = 0;
-    if (f->dot)
-        zprec = ft_max(0, f->precision - dlen);
-    padc = ' ';
-    if (f->zero && !f->minus && !f->dot)
-        padc = '0';
+    zprec = get_zprec(f, dlen);
+    padc = get_padc(f);
     pad = ft_max(0, f->width - pre_len - dlen - zprec);
     count = 0;
     if (!f->minus && padc == ' ')
@@ -26,9 +50,7 @@ int    print_hex_layout(unsigned long nb, const char *base,
         count += putnstr(pre, pre_len);
     if (!f->minus && padc == '0')
         count += putnchar('0', pad);
-    count += putnchar('0', zprec);
-    if (dlen > 0)
-        count += put_unsigned_base(nb, base, 16);
+    count += print_hex_body(nb, base, dlen, zprec);
     if (f->minus)
         count += putnchar(' ', pad);
     return (count);
@@ -36,21 +58,17 @@ int    print_hex_layout(unsigned long nb, const char *base,
 
 int    print_ptr_layout(unsigned long addr, t_format *f)
 {
-    int    dlen;
-    int    zprec;
-    int    pad;
+    int        dlen;
+    int        zprec;
+    int        pad;
     char    padc;
-    int    count;
+    int        count;
 
     dlen = 1;
     if (addr != 0)
         dlen = ulen_base(addr, 16);
-    zprec = 0;
-    if (f->dot)
-        zprec = ft_max(0, f->precision - dlen);
-    padc = ' ';
-    if (f->zero && !f->minus && !f->dot)
-        padc = '0';
+    zprec = get_zprec(f, dlen);
+    padc = get_padc(f);
     pad = ft_max(0, f->width - 2 - dlen - zprec);
     count = 0;
     if (!f->minus && padc == ' ')
