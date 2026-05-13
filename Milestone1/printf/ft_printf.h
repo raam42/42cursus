ef0c8d3670347f4548fa44b52f915d0927011511
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-05-13 20:32:18 +0000

    header updated

--- Milestone1/printf/ft_printf.h
+++ Milestone1/printf/ft_printf.h
@@ -1,10 +1,18 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stddef.h>
+# include <unistd.h>
 # include <stdarg.h>
-# include "libft.h"
+# include "libft/libft.h"
 
 int	ft_printf(const char *format, ...);
+
+int ft_formats(va_list args, const char format);
+int ft_print_char(int c);
+int ft_print_str(char *str);
+int ft_print_ptr(unsigned long long ptr);
+int ft_print_nbr(int n);
+int ft_print_unsigned(unsigned int n);
+int ft_print_hex(unsigned int n, char format);
 
 #endif
