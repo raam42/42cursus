843ae474604e975ded2a2ecb52b9c5a0a1f23f38
rodrigoa <rodrigoa@student.42madrid.com>
2026-05-15 13:14:20 +0200

    ex0 and ex1 added

---
+++ Milestone2/Py00/ex1/ft_garden_name.py
@@ -0,0 +1,16 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_garden_name.py                                  :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rodrigoa <rodrigoa@student.42madrid.c      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/05/15 13:07:46 by rodrigoa          #+#    #+#              #
+#    Updated: 2026/05/15 13:10:50 by rodrigoa         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def	ft_garden_name():
+	name = input("Enter garden name: ")
+	print(f"Garden: {name}")
+	print("Status: Growing well!")
