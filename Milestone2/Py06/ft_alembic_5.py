984b8e8765b4f704287fe86481b9da0c061991a2
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-07-23 14:12:16 +0000

    fixed syntax error

--- Milestone2/Py06/ft_alembic_5.py
+++ Milestone2/Py06/ft_alembic_5.py
@@ -4,4 +4,4 @@ from alchemy import create_air
 if __name__ == "__main__":
     print("=== Alembic 5 ==="
           "Accessing the alchemy module using 'from alchemy import ...'"
-          f"Testing create_air: {create_air}")
+          f"Testing create_air: {create_air()}")
