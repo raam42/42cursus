0d9ce39aa61bfec0564c24b7c73e9bf875b9f1ed
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-07-24 13:30:43 +0000

    updated the import structure

--- Milestone2/Py06/alchemy/potions.py
+++ Milestone2/Py06/alchemy/potions.py
@@ -1,12 +1,14 @@
 import elements
-import Py06.elements
+import alchemy.elements
 
 
 def healing_potion() -> str:
+    """Brew a healing potion using Earth and Air."""
     return (f"Healing potion brewed with"
             f"{elements.create_earth()} and {elements.create_air()}.")
 
 
 def strength_potion() -> str:
+    """Brew a strength potion using Fire and Water."""
     return (f"Strength potion brewed with"
-            f"{Py06.elements.create_fire} and {Py06.elements.create_water}.")
+            f"{elements.create_fire} and {elements.create_water}.")
