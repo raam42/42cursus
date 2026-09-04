1664e08271648cecb7264cd3b32e0cbbb917f110
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-09-04 10:13:31 +0000

    all exercises ready for testing and mypy/flake8 checks

--- Milestone2/Py10/ex4/decorator_mastery.py
+++ Milestone2/Py10/ex4/decorator_mastery.py
@@ -1,7 +1,17 @@
+import sys
 import time
 import functools
 from collections.abc import Callable
 from typing import Any
+
+
+try:
+    from data_generator import FuncMageDataGenerator    # type:  ignore
+except ImportError as e:
+    print(f"[ERROR] Couldn't import 'data_generator.py'.\n"
+          "Please ensure the file is extracted and in the same directory.\n"
+          f"{e}")
+    sys.exit(1)
 
 
 def spell_timer(func: Callable) -> Callable:
@@ -18,7 +28,6 @@ def spell_timer(func: Callable) -> Callable:
         # Format to exactly 3 decimal places as requested
         print(f"Spell completed in {end_time - start_time:.3f} seconds")
         return result
-        
     return wrapper
 
 
@@ -57,8 +66,62 @@ def retry_spell(max_attempts: int) -> Callable:
                     if attempt < max_attempts:
                         print(f"Spell failed, retrying... (attempt {attempt}/{max_attempts})")
             
-            return f"Spell casting failed after {max_attempts} attempts"
+            return (f"Spell casting failed after {max_attempts} attempts"
+                    "\nWaaaaaagh spelled !")
         return wrapper
     return decorator
 
 
+class MageGuild:
+    @staticmethod
+    def validate_mage_name(name: str) -> bool:
+        if len(name) < 3:
+            return False
+        return name.replace(" ", "").isalpha()
+
+    @power_validator(min_power=10)
+    def cast_spell(self, spell_name: str, power: int) -> str:
+        return f"Successfully cast {spell_name} with {power} power"
+
+
+@spell_timer
+def fireball() -> str:
+    time.sleep(0.101)
+    return "Result: Fireball cast!\n"
+
+@retry_spell(max_attempts=3)
+def unstable_spell() -> str:
+    raise RuntimeError("Critical magical failure!")
+
+
+def main() -> None:
+    print("      Master's Tower QA Test Runner      \n",
+          ("=" * 42))
+
+    generator = FuncMageDataGenerator
+    valid_mages = generator.MAGE_NAMES
+    invalid_names = ['Jo', 'A', 'Alex123', 'Test@Name']
+
+    print("\nTesting spell timer...")
+    print(fireball())
+
+    print("Testing retrying spell...")
+    print(unstable_spell())
+
+    print("\nTesting MageGuild...")
+    guild = MageGuild()
+
+    import random
+    test_valid = random.choice(valid_mages)
+    test_invalid = random.choice(invalid_names)
+
+    print(f"Is '{test_valid}' valid?"
+          f" {MageGuild.validate_mage_name(test_valid)}\n"
+          f"Is '{test_invalid}' valid?"
+          f" {MageGuild.validate_mage_name(test_invalid)}\n"
+          f"{guild.cast_spell("Lightning", 15)}\n"
+          f"{guild.cast_spell("Lightning", 5)}")
+
+
+if __name__ == "__main__":
+    main()
